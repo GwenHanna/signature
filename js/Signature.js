f86fc13cc9cf64899cee20bfdcf70284f4e1f21f
Gwen Hanna <gwendoline.garlet@live.fr>
2022-09-29 10:10:01 +0200

    first commit

--- js/Signature.js
+++ js/Signature.js
@@ -1,44 +1,56 @@
 class Signature{
     constructor(canvas){
-        this.sign = false
-        this.prevX = 0
-        this.prevY = 0
+        // Initialisation
+        this.sign = false;
+        this.prevX = 0;
+        this.prevY = 0;
 
-        this.canvas = document.querySelector(canvas)
-        this.ctx = this.canvas.getContext("2d")
-        this.ctx.strokeStyle = "black"
-        this.ctx.lineWidth = 2
-        
+        this.canvas = document.querySelector("canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.ctx.strokeStyle = "black";
+        this.ctx.lineWidth = 2;
+
+        // Ecouteur d'event au moment du click
         this.canvas.addEventListener("mousedown", (e) => {
             // Je signe
-            this.sign = true
-            
-            // Je stocke mes coordonnées de départ
-            this.prevX = e.clientX - this.canvas.offsetLeft
-            this.prevY = e.clientY - this.canvas.offsetTop
+            this.sign = true;
+            console.log(e);
+
+            //Je stock mes coordonée
+            this.prevX = e.clientX - this.canvas.offsetLeft;
+            this.prevY = e.clientY - this.canvas.offsetTop;
+            console.log(this);
         })
 
+        //Ecouteur d'event souri reste appuyer
         this.canvas.addEventListener("mousemove", (e) => {
             // Si je signe
             if(this.sign){
-                let currX = e.clientX - this.canvas.offsetLeft
-                let currY = e.clientY - this.canvas.offsetTop
-                this.draw(this.prevX, this.prevY, currX, currY)
-                this.prevX = currX
-                this.prevY = currY
+                // Déclaration currX et currY sur les coordonnée client 
+                let currX = e.clientX - this.canvas.offsetLeft;
+                let currY = e.clientY - this.canvas.offsetTop;
+                // On applique la méthode du dessin
+                this.draw(this.prevX, this.prevY, currX, currY);
+                // On stock les nouvelle coordonnée sur prevY et prevX
+                this.prevX = currX;
+                this.prevY = currY;
             }
         })
 
+        // Ecouteur d'event relache la souri
         this.canvas.addEventListener("mouseup", () => {
-            this.sign = false
+            this.sign = false 
         })
-
-        this.canvas.addEventListener("mouseout", () => {
-            this.sign = false
+        // Ecouteur d'event on sort du canvas
+        this.canvas.addEventListener("mouseout" , () => {
+            this.sign = false 
         })
     }
 
-    draw(depX, depY, destX, destY){
+    // METHODES
+
+    // Methode dessin
+      draw(depX, depY, destX, destY){
         this.ctx.beginPath()
         this.ctx.moveTo(depX, depY)
         this.ctx.lineTo(destX, destY)
@@ -46,12 +58,21 @@ class Signature{
         this.ctx.stroke()
     }
 
-    effacer(){
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    //Methode effacer
+    clear(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    genererImage(){
-        let image = this.canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
-        return image;
+    // Méthode enregistrer
+    register(){
+        // Pour telecharger la signature coté client(PC)
+        /* let image = this.canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
+        location.href = image;
+        */
+
+         // Pour telecharger la signature coté serveur(SERVEUR)
+         let image = this.canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
+         
+         return image;
     }
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 window.onload = () => {
-    let canvas = new Signature("#signature")
 
-    document.querySelector("#effacer").addEventListener("click", (e) => {
-        e.preventDefault()
-        canvas.effacer()
+    let canvas = new Signature("#signature");
+
+    document.querySelector("#clear").addEventListener("click", (e) => {
+        e.preventDefault();
+        canvas.clear();
     })
 
-    document.querySelector("#enregistrer").addEventListener("click", (e) => {
-        e.preventDefault()
-        
+    document.querySelector("#register").addEventListener("click", (e) => {
+        e.preventDefault();
+      
+
         let data = {
-            image: canvas.genererImage()
+            image : canvas.register()
         }
 
-        fetch("signature.php", {
-            method: "POST",
-            body: JSON.stringify(data)
+        fetch("s.php", {
+            method : "POST",
+            body : JSON.stringify(data)
         }).then((reponse) => {
-            console.log(reponse)
-        })
-    })
-}
+            console.log(reponse);
+        }) 
+    })           
+}  
